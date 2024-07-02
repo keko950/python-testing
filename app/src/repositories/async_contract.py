e00263375118dcd92d60811f7b21cc05bc57ba9f
Gilberto Plaza <keko950@hotmail.com>
2024-07-02 02:30:02 +0200

    Final comments

--- app/src/repositories/async_contract.py
+++ app/src/repositories/async_contract.py
@@ -1,10 +1,11 @@
 from sqlalchemy.orm import session
 from app.src.tables.contract_table import ContractTable
 from app.src.models.contract import Contract
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class AsyncContractORM:
-    def __init__(self, session: session.Session):
+    def __init__(self, session: AsyncSession):
         self.session = session
 
     async def insert_contract(self, contract: Contract):
@@ -13,10 +14,10 @@ class AsyncContractORM:
             contract_type=contract.contract_type,
             name=contract.name
         )
-        self.session.add(item)
+        await self.session.add(item)
 
     async def get_contract(self, contract_id: int) -> Contract:
-        item = self.session.query(ContractTable).filter_by(contract_id=contract_id).first()
+        item = await self.session.query(ContractTable).filter_by(contract_id=contract_id).first()
         return Contract(
             contract_id=item.contract_id,
             contract_type=item.contract_type,
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,26 @@
 import pytest
 
+"""conftest.py
+
+This file serves a critical role configurin and sharing fixtures, hooks,
+and plugins across multiple test files. It is automatically discovered by pytest.
+
+Fixtures are key features in pytest used to set up some precodintiions
+for tests. 
+
+conftest.py allow these fixtures to be shared across multiple test modules.
+
+"""
+
 
 @pytest.fixture
 def test_fixture():
     print("called before each test function that requires this fixture")
     return 1
+
+
+# Custom hook
+def pytest_runtest_setup(item):
+    print(f"setting up {item}")
+    if "slow" in item.keywords and not item.config.getoption("--runslow"):
+        pytest.skip("Skipping slow tests")
